cdefff3beed2da6bf441d3ba0041a7dc9ee2694f
Kingswow <hossein.abavi@gmail.com>
2021-08-27 13:18:37 +0430

    Compile Fix

--- src/common/Cryptography/OpenSSLCrypto.cpp
+++ src/common/Cryptography/OpenSSLCrypto.cpp
@@ -5,13 +5,14 @@
  */
 
 #include <OpenSSLCrypto.h>
+#include <openssl/crypto.h>
 
 #if defined(OPENSSL_VERSION_NUMBER) && OPENSSL_VERSION_NUMBER < 0x1010000fL
-#include <vector>
-#include <thread>
 #include <mutex>
+#include <thread>
+#include <vector>
 std::vector<std::mutex*> cryptoLocks;
-static void lockingCallback(int mode, int type, char const* /*file*/, int /*line*/)
+static void              lockingCallback(int mode, int type, char const* /*file*/, int /*line*/)
 {
     if (mode & CRYPTO_LOCK)
     {
@@ -24,26 +25,26 @@ static void lockingCallback(int mode, int type, char const* /*file*/, int /*line
 }
 static void threadIdCallback(CRYPTO_THREADID* id)
 {
-    (void)id;
+    (void) id;
     CRYPTO_THREADID_set_numeric(id, std::hash<std::thread::id>()(std::this_thread::get_id()));
 }
 void OpenSSLCrypto::threadsSetup()
 {
     cryptoLocks.resize(CRYPTO_num_locks());
-    for (int i = 0 ; i < CRYPTO_num_locks(); ++i)
+    for (int i = 0; i < CRYPTO_num_locks(); ++i)
     {
         cryptoLocks[i] = new std::mutex();
     }
-    (void)&threadIdCallback;
+    (void) &threadIdCallback;
     CRYPTO_THREADID_set_callback(threadIdCallback);
-    (void)&lockingCallback;
+    (void) &lockingCallback;
     CRYPTO_set_locking_callback(lockingCallback);
 }
 void OpenSSLCrypto::threadsCleanup()
 {
     CRYPTO_set_locking_callback(nullptr);
     CRYPTO_THREADID_set_callback(nullptr);
-    for (int i = 0 ; i < CRYPTO_num_locks(); ++i)
+    for (int i = 0; i < CRYPTO_num_locks(); ++i)
     {
         delete cryptoLocks[i];
     }
